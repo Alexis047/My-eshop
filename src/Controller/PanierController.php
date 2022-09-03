f9ff6297fcbea4f5c7f03d0d7d19607f4a04976e
Alexis047 <alexiclement@gmail.com>
2022-09-03 14:44:13 +0200

    init commit

---
+++ src/Controller/PanierController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Produit;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class PanierController extends AbstractController
+{
+    #[Route('/voir-mon-panier', name: 'show_panier', methods: ['GET'])]
+    public function showPanier(SessionInterface $session): Response
+    {
+        $panier = $session->get('panier', []);
+        $total = 0;
+
+        foreach($panier as $item) {
+            $totalItem = $item['produit']->getPrice() * $item['quantity'];
+            # On ajoute ce total au montant final
+            $total += $totalItem;
+        }
+
+        return $this->render('panier/show_panier.html.twig', [
+            'total' => $total
+        ]);
+    } // end function showPanier()
+
+    #[Route('/ajouter-au-panier/{id}', name: 'add_item', methods: ['GET'])]
+    public function addItem(Produit $produit, SessionInterface $session): Response
+    {
+        # Si dans la $session le panier n'existe pas, alors la méthode get() retournera le second paramètre, un array vide.
+        $panier = $session->get('panier', []);
+
+        if(!empty($panier[$produit->getId()])) {
+            ++$panier[$produit->getId()]['quantity'];
+        } else {
+            $panier[$produit->getId()]['quantity'] = 1;
+            $panier[$produit->getId()]['produit'] = $produit;
+        }
+
+        # Ici, nous devons set() le panier en session, en lui passant $panier[]
+        $session->set('panier', $panier);
+
+        $this->addFlash('success', 'Le produit a bien été ajouté à votre panier :)');
+        return $this->redirectToRoute('default_home');
+    } // end function add_item
+}
